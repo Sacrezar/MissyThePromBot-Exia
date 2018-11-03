c1cd0697bd8f33d824da6b2fdef8bfb27ea275c7
Sacrezar <thomas.soulas@viacesi.fr>
2018-11-03 15:58:23 +0100

    cog setup

--- bot.py
+++ bot.py
@@ -1,116 +1,65 @@
 import os
+import inspect
 
-import discord
-import time
 import json
-from assignation import Assignation_roles_random
-from functions import getDateInList
+import discord
 from discord.ext import commands
-# from functions import getDay
-#test
 
 with open('config.json') as f:
     config = json.load(f)
 
 bot = commands.Bot(description=config.get('desc'), command_prefix=config.get('prefix'))
+bot.remove_command('help')
 owner = config.get('owners')
 
+exts = []
+cfile = []
+print("cogs is filled with : ", end="")
+for file in os.listdir("cogs"):
+    if file.endswith(".py"):
+        print(file, end=" - ")
+        l = list('cogs.{}'.format(file))[0:-3]
+        cfiles = "".join(l)
+        exts.append(cfiles)
+print(end="")
 
-
+#Online event
 @bot.event
 async def on_ready(): 
-    print("[*] I'm in")
+    print("\n")
+    print("[*] I'm in, {}".format(config.get('join_message')))
     print('[*] Name: {}, Owner: {}'.format(bot.user.name,owner))
     await bot.change_presence(game=discord.Game(name=config.get('playing')))    
 
-#give the date
+#Load & Unlod commands
 @bot.command(pass_context=True)
-async def date(ctx):
-    date = " ".join(getDateInList())
-    await bot.send_message(ctx.message.channel, "```xl\n'{}'\n```".format(date))
+async def load(ctx, exts):
+    if(ctx.message.author.id == owner):
+        try:
+            bot.load_extension(extension)
+            print('Loaded {}'.format(extension))
+        except Exception as e:
+            print('{} cannot be loaded. [{}]'.format(extension, e))
+    else:
+        print('{} tried to use {}'.format(ctx.message.author, inspect.stack()[0][3]))
 
-#Group Rotation (Tuesday & Wednesday)
 @bot.command(pass_context=True)
-async def roll(ctx):
-    listG1 = []
-    listG2 = []
-    listG3 = []
-    channel = ctx.message.channel
-
+async def unload(ctx, exts):
     if(ctx.message.author.id == owner):
-        #Groups recovery
-        for member in bot.get_all_members():
-            print("This is: " + member.display_name + " " + member.id + " " + str(member.roles[1]))
-            if(str(member.roles[1]) == "G1"):
-                listG1.append(member.display_name)
-             
-            if(str(member.roles[1]) == "G2"):
-                listG2.append(member.display_name)
-
-            if(str(member.roles[1]) == "G3"):
-                listG3.append(member.display_name)
-
-        print(listG1) 
-        print(listG2)
-        print(listG3)
-
-#         #Calling assignation function
-        assigner = Assignation_roles_random(0, 1, len(listG1)-1)
-#        logger.info("G1 : {}".format(assigner))
-        tirLeader1 = listG1[assigner['leader']]
-        tirSecret1 = listG1[assigner['secretaire']]
-        tirScrib1 = listG1[assigner['scribe']]
-        tirTkeeper1 = listG1[assigner['gestionnaire']]
-        
-        #Calling assignation function
-        assigner = Assignation_roles_random(0, 2, len(listG2)-1)
-#        logger.info("G2 : {}".format(assigner))
-        tirLeader2 = listG2[assigner['leader']]
-        tirSecret2 = listG2[assigner['secretaire']]
-        tirScrib2 = listG2[assigner['scribe']]
-        tirTkeeper2 = listG2[assigner['gestionnaire']]       
-
-        #Calling assignation function
-#         assigner = Assignation_roles_random(0, 3, len(listG3)-1)
-# #        logger.info("G3 : {}".format(assigner))
-#         tirLeader3 = listG3[assigner['leader']]
-#         tirSecret3 = listG3[assigner['secretaire']]
-#         tirScrib3 = listG3[assigner['scribe']]
-#         tirTkeeper3 = listG3[assigner['gestionnaire']]
-        day = "" 
-
-        await bot.send_message(discord.Object(id='499515804589490178'),"⠀\n```fix\n{0}\n```".format(day))
-        await bot.send_message(discord.Object(id='499515804589490178'),"⠀\n<@&374629943918985237>:```prolog\nAnimateur    : '{0}' \nSecretaire   : '{1}' \nScribe       : '{2}' \nGestionnaire : '{3}'\n```".format(tirLeader1, tirSecret1, tirScrib1, tirTkeeper1))
-        await bot.send_message(discord.Object(id='499515804589490178'),"⠀\n<@&374629942371287043>:```prolog\nAnimateur    : '{0}' \nSecretaire   : '{1}' \nScribe       : '{2}' \nGestionnaire : '{3}'\n```".format(tirLeader2, tirSecret2, tirScrib2, tirTkeeper2))
-#         # await bot.send_message(discord.Object(id='499515804589490178'),"⠀\n<@&374629949572907028>:\n ```prolog\nAnimateur    : '{0}' \nSecretaire   : '{1}' \nScribe       : '{2}' \nGestionnaire : '{3}'```\n".format(tirLeader3, tirSecret3, tirScrib3, tirTkeeper3))
+        try:
+            bot.load_extension(extension)
+            print('Unloaded {}'.format(extension))
+        except Exception as e:
+            print('{} cannot be unloaded. [{}]'.format(extension, e))
     else:
-        messages = []
-        await bot.send_message(channel,"`\nYou're not allowed to\n`")
-        time.sleep(.5)
-        async for message in bot.logs_from(channel, 1):
-            messages.append(message)
-        await bot.delete_messages(message)
-        
+        print('{} tried to use {}'.format(ctx.message.author, inspect.stack()[0][3]))
 
-#Clear function
-@bot.command(pass_context = True)
-async def clear(ctx, amount=100):
-    if(ctx.message.author.id == owner):
-        channel = ctx.message.channel
-        messages = []
-        async for message in bot.logs_from(channel, limit=int(amount+1)):
-            messages.append(message)
-        numberOfMessages = len(messages)-1
-        await bot.delete_messages(messages)
-        await bot.send_message(channel,'` {} messages deleted`'.format(numberOfMessages))
-        
-        async for message in bot.logs_from(channel, 1):
-            messages.append(message)
-        await bot.delete_messages(messages)
-    else:
-        await bot.send_message(channel,"`\nYou're not allowed to\n`")
-        async for message in bot.logs_from(channel, 1):
-            messages.append(message)
-        await bot.delete_messages(messages)
+
+if __name__ == '__main__':
+    for extension in exts:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print('{} cannot be loaded. [{}]'.format(extension, e))
     
 bot.run(config.get('token'))
