3f3f250b0615c7a9e32352e234ebfde178daca56
Sacrezar <thomas.soulas@viacesi.fr>
2019-08-15 15:10:14 +0200

    fixing issue with clear

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,5 +1,6 @@
 import os
 import discord
+import time
 
 from discord.ext import commands
 from funs import assignation, date
@@ -19,20 +20,24 @@ class Admin:
     @commands.command(pass_context = True)
     async def clear(self, ctx, amount=99):
         channel = ctx.message.channel
+        messages = []
+
         if q.is_he_admin(ctx.message.server.id,ctx.message.author.id):
-            messages = []
             async for message in self.bot.logs_from(channel, limit=int(amount+1)):
                 messages.append(message)
             numberOfMessages = len(messages)-1
             await self.bot.delete_messages(messages)
-            await self.bot.send_message(channel,'` {} messages deleted`'.format(numberOfMessages))
             
+            await self.bot.send_message(channel,'` {} messages deleted`'.format(numberOfMessages))
+        
             async for message in self.bot.logs_from(channel, 1):
                 messages.append(message)
             await self.bot.delete_messages(messages)
+
         else:
             await self.bot.send_message(channel,"`\nYou're not allowed to\n`")
-            async for message in self.bot.logs_from(channel, 1):
+
+            async for message in self.bot.logs_from(channel, 2):
                 messages.append(message)
             await self.bot.delete_messages(messages)
 
